fc3306bd9fb949113dbc6a356e7795559fdd8692
Gabriel Diaz <gdiaz@qswarm.com>
2018-07-28 08:48:04 +0200

    add kvm accel options

--- vm_create.sh
+++ vm_create.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 set -e 
+set -a
 
 if [ -z "$1" ]; then
 	echo Usage:
-	echo   ${0} slackbios 20G 2048 BIOS user img/slackware64-14.2-install-dvd.iso
-	echo   ${0} slackuefi 20G 2048 UEFI user img/slackware64-14.2-install-dvd.iso
+	echo   ${0} slackbios kvm 20G 2048 BIOS user img/slackware64-14.2-install-dvd.iso
+	echo   ${0} slackuefi kvm 20G 2048 UEFI user img/slackware64-14.2-install-dvd.iso
 	exit 0
 fi
 
@@ -14,7 +15,7 @@ ROOT=`dirname $0`
 cd ${ROOT}
 ROOT=`pwd`
 
-PARAMS=(NAME DISK_SIZE MEM_SIZE BOOT_TYPE NET_TYPE INSTALL_IMAGE)
+PARAMS=(NAME ACCEL DISK_SIZE MEM_SIZE BOOT_TYPE NET_TYPE INSTALL_IMAGE)
 
 for i in "${PARAMS[@]}"; do
 	if [ -z "$1" ]; then
@@ -23,9 +24,15 @@ for i in "${PARAMS[@]}"; do
 	fi
 	declare ${i}=$1; shift
 done
-export BIOS="-bios ${ROOT}/bios/seabios/bios.bin"
+KVM=""
+if [ "$ACCEL" == "kvm" ]; then
+	 # KVM="-enable-kvm -machine q35,accel=kvm -device intel-iommu -cpu host"
+	 KVM="-enable-kvm -cpu host"
+fi
+ 
+BIOS="-bios ${ROOT}/bios/seabios/bios.bin"
 if [ "${BOOT_TYPE}" == "UEFI" ]; then
-	export BIOS="-drive if=pflash,format=raw,readonly,file=../../bios/ovmf-x64/OVMF_CODE-pure-efi.fd -drive if=pflash,format=raw,file=../../bios/ovmf-x64/OVMF_VARS-pure-efi.fd"
+	BIOS="-drive if=pflash,format=raw,readonly,file=../../bios/ovmf-x64/OVMF_CODE-pure-efi.fd -drive if=pflash,format=raw,file=../../bios/ovmf-x64/OVMF_VARS-pure-efi.fd"
 fi
 
 mkdir -p vm/${NAME}
@@ -36,14 +43,14 @@ qemu-img create -f qcow2 vm/${NAME}/disk.img ${DISK_SIZE}
 cat << EOF > vm/${NAME}/install.sh
 #!/usr/bin/env bash
 
-qemu-system-x86_64 ${BIOS} -m ${MEM_SIZE} -cdrom ${INSTALL_IMAGE} -hda disk.img -boot d -net nic -net ${NET_TYPE} -m ${MEM_SIZE} -rtc base=localtime -vga std
+qemu-system-x86_64 ${KVM} ${BIOS} -m ${MEM_SIZE} -cdrom ${INSTALL_IMAGE} -hda disk.img -boot d -net nic -net ${NET_TYPE} -m ${MEM_SIZE} -rtc base=localtime -vga std
 
 EOF
 
 cat << EOF > vm/${NAME}/run.sh
 #!/usr/bin/env bash
 
-qemu-system-x86_64 ${BIOS} -m ${MEM_SIZE} -hda disk.img -boot c -net nic -net ${NET_TYPE} -m ${MEM_SIZE} -rtc base=localtime -vga std
+qemu-system-x86_64 ${KVM} ${BIOS} -m ${MEM_SIZE} -hda disk.img -boot c -net nic -net ${NET_TYPE} -m ${MEM_SIZE} -rtc base=localtime -vga std
 
 EOF
 
